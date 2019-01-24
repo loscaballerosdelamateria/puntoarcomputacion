aa6a19ff1ecdf05db65234a3ecd7f82ccf17d7fd
Alejandro Chandro <ale.chandro@hotmail.com>
2019-01-24 20:19:03 -0300

    Versión 3

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(window).scroll(function(){
+    var barra = $(window).scrollTop();
+    var posicion = barra * 0.20;
+
+    $('body').css({
+        'background-position': '0 -' + posicion + 'px'
+    })
+});
